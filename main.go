7749048b9d992f77331e36006c5e8a8086e4d278
aquilax <aquilax@gmail.com>
2015-09-26 06:34:14 +0200

    Player moves now WOW

--- main.go
+++ main.go
@@ -11,19 +11,89 @@ const (
 	boardWidth      = 8
 	boardHeight     = 8
 	answersPerLevel = 10
+
+	cellWidth   = 7
+	borderWidth = 1
+	offsetX     = 1
+
+	cellHeight   = 3
+	borderHeight = 1
+	offsetY      = 1
 )
 
+// BoardCell contains number pair for solving
 type BoardCell struct {
 	n1 int
 	n2 int
 }
 
+// BoardCellList contains list of number pairs
 type BoardCellList []BoardCell
 
+// Board contains all number pairs for the level
 type Board [][]BoardCell
 
+type Player struct {
+	game   *tl.Game
+	boardX int
+	boardY int
+	entity *tl.Entity
+}
+
+// String returns textual representation of the number pair
 func (bc *BoardCell) String() string {
 	return fmt.Sprintf("%2d+%-2d", bc.n1, bc.n2)
+}
+
+func NewPlayer(game *tl.Game) Player {
+	player := Player{
+		game,
+		0,
+		0,
+		tl.NewEntity(1, 1, 1, 1),
+	}
+	player.entity.SetCell(0, 0, &tl.Cell{Bg: tl.ColorRed, Ch: '옷'})
+	player.entity.SetPosition(player.getPosition())
+	return player
+}
+
+func (player *Player) Draw(screen *tl.Screen) {
+	player.entity.Draw(screen)
+}
+
+func (player *Player) getPosition() (int, int) {
+	x := player.boardX*(cellWidth+borderWidth) + offsetX + borderWidth
+	y := player.boardY*(cellHeight+borderHeight) + offsetY + borderHeight
+	return x, y
+}
+
+func (player *Player) Tick(event tl.Event) {
+	if event.Type == tl.EventKey { // Is it a keyboard event?
+		switch event.Key { // If so, switch on the pressed key.
+		case tl.KeyArrowRight:
+			if player.boardX < boardWidth-1 {
+				player.boardX += 1
+			}
+			break
+		case tl.KeyArrowLeft:
+			if player.boardX > 0 {
+				player.boardX -= 1
+			}
+			break
+		case tl.KeyArrowUp:
+			if player.boardY > 0 {
+				player.boardY -= 1
+			}
+			break
+		case tl.KeyArrowDown:
+			if player.boardY < boardHeight-1 {
+				player.boardY += 1
+			}
+			break
+		}
+		player.game.Log("BoardX=%d\tBoardY=%d", player.boardX, player.boardY)
+		player.entity.SetPosition(player.getPosition())
+	}
 }
 
 // NewBoardCell generates new pair of numbers for the board
@@ -70,17 +140,20 @@ func NewBoard(level int) Board {
 func buildLevel(game *tl.Game, gameLevel, score int) {
 	level := tl.NewBaseLevel(tl.Cell{})
 	game.Screen().SetLevel(level)
+	// Add chrome
 	scoretext := tl.NewText(50, 0, "Score: "+strconv.Itoa(score),
 		tl.ColorWhite, tl.ColorBlack)
 	game.Screen().AddEntity(tl.NewText(1, 0, " Number crusader! ", tl.ColorBlack, tl.ColorGreen))
 	game.Screen().AddEntity(scoretext)
 
+	// Create the level background
 	level.AddEntity(tl.NewRectangle(1, 1, 65, 33, tl.ColorGreen))
 	for i := 2; i < 63; i = i + 8 {
 		for j := 2; j < 31; j = j + 4 {
 			level.AddEntity(tl.NewRectangle(i, j, 7, 3, tl.ColorBlue))
 		}
 	}
+	// Add the numbers
 	board := NewBoard(gameLevel)
 	for i := range board {
 		for j := range board[i] {
@@ -89,6 +162,9 @@ func buildLevel(game *tl.Game, gameLevel, score int) {
 			game.Screen().AddEntity(tl.NewText(x, y, board[i][j].String(), tl.ColorWhite, tl.ColorBlue))
 		}
 	}
+	player := NewPlayer(game)
+	level.AddEntity(&player)
+
 }
 
 func main() {
